f07d85a27985d7b8eaede5afb352b7192258a2c3
Carson Case <carsoncase@gmail.com>
2022-07-26 21:42:11 +0200

    liquidations

--- contracts/Strategy.sol
+++ contracts/Strategy.sol
@@ -38,6 +38,12 @@ contract Strategy is ERC20{
         boardwalkToken = _boardwalkToken;
         dex = _dex;
         swaps = _swaps;
+
+        stablecoin.approve(address(dex), 2**256-1);
+        streamCoin.approve(address(dex), 2**256-1);
+        asset.approve(address(dex), 2**256-1);
+        boardwalkToken.approve(address(dex), 2**256-1);
+
     }
     /*
     Each Strategy has 4 categories of funds in it
@@ -93,8 +99,11 @@ contract Strategy is ERC20{
             availableCollateral[msg.sender]) >= (MIN_COLLATERAL * amountCash) / ONE_HUNDRED_PERCENT,
              "Not enough collateral to buy this swap"
              );
+
+        availableCollateral[msg.sender] -= _getBordValue((MIN_COLLATERAL * amountCash) / ONE_HUNDRED_PERCENT);
         // swap cash to asset
         require(getCashFunds() >= amountCash, "Not enough cash in contract to purchase this swap");
+
         address[] memory path = new address[](2);
         path[0] = address(stablecoin);
         path[1] = address(asset);
@@ -111,6 +120,10 @@ contract Strategy is ERC20{
     }
 
     function settle(uint underlyingAmount, uint startingStablePrice, address payer) external{
+        // unlock collateral
+        uint colLocked = _getBordValue((MIN_COLLATERAL * startingStablePrice) / ONE_HUNDRED_PERCENT);
+        availableCollateral[msg.sender] += colLocked;
+
         //f irst trade underlying back for stables and capture the amounts
         address[] memory path = new address[](2);
         path[0] = address(asset);
@@ -127,16 +140,37 @@ contract Strategy is ERC20{
         // if larger then return difference to the swap buyer
         if(amounts[1] > startingStablePrice){
             stablecoin.transfer(payer, amounts[1]-startingStablePrice);
+        }else{
+            // if smaller then remove then liquidate appropriate amount of collateral from the buyer
+            uint colStableValue = _getCashValue(address(boardwalkToken), availableCollateral[msg.sender]);
+            if(colStableValue >= startingStablePrice-amounts[1]){
+                _liquidate(msg.sender, startingStablePrice-amounts[1]);
+            }else{
+                // if there's not enough collateral then liquidate all of it
+                _liquidate(msg.sender, colStableValue);
+            }
         }
 
-        // if smaller then remove then liquidate appropriate amount of collateral from the buyer
-        // if there's not enough collateral then liquidate all of it
-        /// todo
     }
 
     function depositCollateral(uint amount) external{
         boardwalkToken.transferFrom(msg.sender, address(this), amount);
         availableCollateral[msg.sender] += amount;
+    }
+
+    function _liquidate(address _toLiquidate, uint _amountBord) internal{
+        availableCollateral[_toLiquidate] -= _amountBord;
+        address[] memory path = new address[](2);
+        path[0] = address(boardwalkToken);
+        path[1] = address(stablecoin);
+        uint[] memory amounts = dex.swapExactTokensForTokens(
+            _amountBord,
+            _getMinOut(_amountBord, path),
+            path, 
+            address(this), 
+            block.timestamp+30
+        );
+
     }
 
     function _getMinOut(uint _amountIn, address[] memory _path) internal view returns(uint minOut){
@@ -148,6 +182,14 @@ contract Strategy is ERC20{
         address[] memory path = new address[](2);
         path[0] = token;
         path[1] = address(stablecoin);
+        uint[] memory amountsOut = dex.getAmountsOut(amount, path);
+        return(amountsOut[1]);
+    }
+
+    function _getBordValue(uint amount) internal view returns(uint cashVal){
+        address[] memory path = new address[](2);
+        path[0] = address(stablecoin);
+        path[1] = address(boardwalkToken);
         uint[] memory amountsOut = dex.getAmountsOut(amount, path);
         return(amountsOut[1]);
     }
